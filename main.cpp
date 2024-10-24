864c8b0da61732e16ce8538c534dd1c0be5e9e3a
vickvey <vivek22@iiserb.ac.in>
2024-10-24 16:10:06 +0530

    added basic cplusplus template

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#define MOD (ll)(1e9+7)
+#define MAX(x, y) (x > y ? x : y)
+#define MIN(x, y) (x < y ? x : y)
+using namespace std;
+using ll = long long;
+
+void solve();
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t = 0;
+    scanf("%lld", &t);
+    while(t--) solve();
+    return 0;
+}
+/////////////////////////////////////////////////////////////////
+// CODE HERE
+/////////////////////////////////////////////////////////////////
+void solve() {
+}
